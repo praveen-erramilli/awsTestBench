391ee3ff15eb8b240a83dd3ffa24588b6bd2001d
Praveen Erramilli <epraveenns@gmail.com>
2019-12-05 15:30:48 +0530

    HelloWorld AWS

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,51 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+
+public class HelloWorld implements RequestHandler<HelloWorld.RequestClass, HelloWorld.ResponseClass> {
+
+    @Override
+    public ResponseClass handleRequest(RequestClass req, Context context) {
+        ResponseClass responseClass = new ResponseClass();
+        responseClass.greeting = "HelloWorld "+ req.firstName + req.lastName;
+        return responseClass;
+    }
+
+
+    public static class RequestClass
+    {
+        private String firstName;
+        private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
+
+    public static class ResponseClass
+    {
+        private String greeting;
+
+        public String getGreeting() {
+            return greeting;
+        }
+
+        public void setGreeting(String greeting) {
+            this.greeting = greeting;
+        }
+    }
+}
